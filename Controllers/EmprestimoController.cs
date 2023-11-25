5f7811ab24ddbd787e1d11cba80350092296bfa7
Alexander <alexander.oliveira3@etec.sp.gov.br>
2023-11-25 17:21:03 -0300

    Correções2

--- Controllers/EmprestimoController.cs
+++ Controllers/EmprestimoController.cs
@@ -1,8 +1,10 @@
-using Microsoft.AspNetCore.Mvc;
 using BibliotecaMvc.Models;
-using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-
+using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
 
 namespace BibliotecaMvc.Controllers
 {
@@ -88,20 +90,34 @@ namespace BibliotecaMvc.Controllers
         {
             try
             {
+                // Verificar se os IDs são válidos
+                if (IdLivro <= 0 || IdUsuario <= 0)
+                {
+                    TempData["MensagemErro"] = "IDs de Livro e Usuário devem ser maiores que zero.";
+                    return RedirectToAction("Create");
+                }
+
+                // Log de informações
+                Console.WriteLine($"Recebendo solicitação para realizar empréstimo - ID Livro: {IdLivro}, ID Usuário: {IdUsuario}");
+
                 using (HttpClient httpClient = new HttpClient())
                 {
-                    var emprestimo = new EmprestismoDeLivros
-                    {
-                        IdLivro = IdLivro,
-                        IdUsuario = IdUsuario
-                        // Você pode definir as outras propriedades, se necessário
-                    };
+                    // Criar um objeto anônimo com os IDs
+                    var emprestimo = new { IdLivro = IdLivro, IdUsuario = IdUsuario };
 
+                    // Log de informações
+                    Console.WriteLine($"Enviando solicitação para API - ID Livro: {emprestimo.IdLivro}, ID Usuário: {emprestimo.IdUsuario}");
+
+                    // Serializar o objeto em JSON
                     var content = new StringContent(JsonConvert.SerializeObject(emprestimo));
+
+                    // Definir o cabeçalho de tipo de conteúdo
                     content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
+                    // Enviar a solicitação POST para a API
                     HttpResponseMessage response = await httpClient.PostAsync(uriBase + "realizarEmprestimo", content);
 
+                    // Verificar se a solicitação foi bem-sucedida
                     if (response.IsSuccessStatusCode)
                     {
                         var serialized = await response.Content.ReadAsStringAsync();
@@ -110,17 +126,20 @@ namespace BibliotecaMvc.Controllers
                     }
                     else
                     {
-                        TempData["MensagemErro"] = "Ocorreu um erro ao realizar o empréstimo.";
+                        TempData["MensagemErro"] = $"Erro ao realizar o empréstimo. Resposta da API: {response.ReasonPhrase}";
                         return RedirectToAction("Create");
                     }
                 }
             }
             catch (Exception ex)
             {
+                // Log de erro
+                Console.WriteLine($"Erro ao realizar o empréstimo: {ex.Message}");
                 TempData["MensagemErro"] = ex.Message;
                 return RedirectToAction("Create");
             }
         }
+
 
         [HttpGet]
         public async Task<ActionResult> Edit(int? IdTransacao)
@@ -137,14 +156,21 @@ namespace BibliotecaMvc.Controllers
         }
 
         [HttpPost]
-        public async Task<ActionResult> Edit(int IdLivro, int IdUsuario)
+        public async Task<ActionResult> Devolver(int IdLivro, int IdUsuario)
         {
             try
             {
+                // Verificar se os IDs são válidos
+                if (IdLivro <= 0 || IdUsuario <= 0)
+                {
+                    TempData["MensagemErro"] = "IDs de Livro e Usuário devem ser maiores que zero.";
+                    return RedirectToAction("Index");
+                }
+
                 using (HttpClient httpClient = new HttpClient())
                 {
-                    var emprestimo = new { IdLivro = IdLivro, IdUsuario = IdUsuario };
-                    var content = new StringContent(JsonConvert.SerializeObject(emprestimo));
+                    var devolucao = new { IdLivro = IdLivro, IdUsuario = IdUsuario };
+                    var content = new StringContent(JsonConvert.SerializeObject(devolucao));
                     content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
 
                     HttpResponseMessage response = await httpClient.PutAsync(uriBase + $"realizarDevolucao", content);
@@ -163,11 +189,12 @@ namespace BibliotecaMvc.Controllers
             }
             catch (Exception ex)
             {
+                // Log de erro
+                Console.WriteLine($"Erro ao realizar a devolução: {ex.Message}");
                 TempData["MensagemErro"] = ex.Message;
                 return RedirectToAction("Index");
             }
         }
-
 
         [HttpPost]
         public async Task<ActionResult> Delete(int IdTransacao)
@@ -192,6 +219,8 @@ namespace BibliotecaMvc.Controllers
             }
             catch (Exception ex)
             {
+                // Log de erro
+                Console.WriteLine($"Erro ao excluir o empréstimo: {ex.Message}");
                 TempData["MensagemErro"] = ex.Message;
                 return RedirectToAction("Index");
             }
